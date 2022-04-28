a9911202efe495529c5790f148284ec872c18aa7
Aleksandr-Pod <stdfire@gmail.com>
2022-04-28 21:37:49 +0300

    Movies added

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,22 @@
-export default function MoviesPage() {
+import { useState, useEffect } from "react";
+import * as movieApi from './APIfetch/APIfetch';
+import { TodayMoviesList, TodayMoviesTitle, MoviesItem } from '../components/TodayMovies/todayMovies-styled';
+import { NavLink } from 'react-router-dom';
 
- // movieApi.fetchSearch('batman').then(data => setMovies(data.results));
+export default function MoviesPage() {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        movieApi.fetchSearch('batman').then(data => setMovies(data.results));
+    })
+
     return (
-        <>
-            Список фильмов по названию
-        </>
+        <TodayMoviesList>
+            <TodayMoviesTitle>Список фильмов за день</TodayMoviesTitle>
+            {movies.map(el => (
+                <MoviesItem key={el.id}>
+                    <NavLink to={`movies/${el.id}`}>{el.title}</NavLink>
+                </MoviesItem>
+            ))}
+        </TodayMoviesList>
     )
 }
