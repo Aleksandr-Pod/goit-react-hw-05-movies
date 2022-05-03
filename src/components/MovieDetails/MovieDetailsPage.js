843117d955b2de23eae256d54c0aa17dbfee3e51
Aleksandr-Pod <stdfire@gmail.com>
2022-05-03 08:34:08 +0300

    GoBackBtn

--- src/components/MovieDetails/MovieDetailsPage.js
+++ src/components/MovieDetails/MovieDetailsPage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { NavLink, useParams, Outlet, useNavigate } from "react-router-dom";
+import { NavLink, useParams, Outlet, useNavigate, useLocation } from "react-router-dom";
 import * as APIfetch from '../APIfetch/APIfetch';
 import { MovieDetailsBlock, MovieDetailsInfo } from './MovieDetails-styled';
 
@@ -7,16 +7,21 @@ export default function MovieDetailsPage() {
     const { movieId } = useParams();
     const [movie, setMovie] = useState();
     const navigate = useNavigate();
+    const location = useLocation();
+    console.log(location);
 
     useEffect(() => {
         APIfetch.fetchMovieById(movieId).then(setMovie)
     }, [movieId, setMovie]);
 
-     
+    const onGoBack = () => {
+        navigate(location?.state?.from ?? "/");
+    }
+    
     return (
         <>{ movie && 
             <>
-            <button type="button" onClick={() => navigate(-1)}>Go back</button>
+            <button type="button" onClick={onGoBack}>Go back</button>
             <MovieDetailsBlock>
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     alt={movie.title} width="240"></img>
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,13 +1,19 @@
 import { MoviesBox, MoviesTitle, MoviesItem } from './Movies-styled';
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function MoviesList({ movies, title, prePath }) {
+    const location = useLocation();
+    console.log(location);
+
     return (
         <MoviesBox>
             <MoviesTitle>{title}</MoviesTitle>
             {movies.map(el => (
                 <MoviesItem key={el.id}>
-                    <NavLink to={`${prePath}${el.id}`}>{el.title}</NavLink>
+                    <Link to={{
+                        pathname: `${prePath}${el.id}`,
+                        state: { from: location }
+                    }}>{el.title}</Link>
                 </MoviesItem>
             ))}
         </MoviesBox>
